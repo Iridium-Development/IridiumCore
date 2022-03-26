429d9506b74b16165fbd92ad5357755bd4cd3c61
Peaches_MLG <PeachesMLG100@gmail.com>
2022-03-26 21:34:33 +0000

    1.5.5

--- plugin/src/main/java/com/iridium/iridiumcore/gui/PagedGUI.java
+++ plugin/src/main/java/com/iridium/iridiumcore/gui/PagedGUI.java
@@ -5,6 +5,7 @@ import com.iridium.iridiumcore.Item;
 import com.iridium.iridiumcore.utils.InventoryUtils;
 import com.iridium.iridiumcore.utils.ItemStackUtils;
 import lombok.AllArgsConstructor;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
@@ -16,7 +17,8 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public abstract class PagedGUI<T> implements GUI {
 
-    private final int page;
+    private int page;
+    private final int size;
     private final Background background;
     private final Item previousPage;
     private final Item nextPage;
@@ -25,10 +27,12 @@ public abstract class PagedGUI<T> implements GUI {
     public void addContent(Inventory inventory) {
         InventoryUtils.fillInventory(inventory, background);
 
-        inventory.setItem(inventory.getSize() - 3, ItemStackUtils.makeItem(nextPage));
-        inventory.setItem(inventory.getSize() - 7, ItemStackUtils.makeItem(previousPage));
+        if (isPaged()) {
+            inventory.setItem(inventory.getSize() - 3, ItemStackUtils.makeItem(nextPage));
+            inventory.setItem(inventory.getSize() - 7, ItemStackUtils.makeItem(previousPage));
+        }
 
-        int elementsPerPage = inventory.getSize() - 9;
+        int elementsPerPage = inventory.getSize() - (isPaged() ? 9 : 0);
         List<T> objects = getPageObjects().stream()
                 .skip((long) (page - 1) * elementsPerPage)
                 .limit(elementsPerPage)
@@ -42,4 +46,31 @@ public abstract class PagedGUI<T> implements GUI {
     public abstract Collection<T> getPageObjects();
 
     public abstract ItemStack getItemStack(T t);
+
+    public int getSize() {
+        if (size <= 0) {
+            return Math.min((int) (Math.round(size / 9.0) * 9), 56);
+        }
+        return size;
+    }
+
+    public boolean isPaged() {
+        return getPageObjects().size() > getSize();
+    }
+
+    @Override
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (!isPaged()) return;
+        if (event.getSlot() == getInventory().getSize() - 7) {
+            if (page > 1) {
+                page--;
+                event.getWhoClicked().openInventory(getInventory());
+            }
+        } else if (event.getSlot() == getInventory().getSize() - 3) {
+            if ((event.getInventory().getSize() - 9) * page < getPageObjects().size()) {
+                page++;
+                event.getWhoClicked().openInventory(getInventory());
+            }
+        }
+    }
 }
