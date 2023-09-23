9edb59c1194975f7f96912a0457beacfb7b61323
Peaches_MLG <PeachesMLG100@gmail.com>
2023-09-23 15:25:45 +0100

    Added TimeUtils - Version Increment

---
+++ plugin/src/main/java/com/iridium/iridiumcore/utils/TimeUtils.java
@@ -0,0 +1,17 @@
+package com.iridium.iridiumcore.utils;
+
+import java.time.Duration;
+
+public class TimeUtils {
+
+    public static String formatDuration(String format, Duration duration) {
+        long hours = duration.toHours();
+        long minutes = duration.toMinutes() % 60;
+        long seconds = duration.getSeconds() % 60;
+
+        return format.replace("%hours%", String.valueOf(hours))
+                .replace("%minutes%", String.valueOf(minutes))
+                .replace("%seconds%", String.valueOf(seconds));
+    }
+
+}
