78c8ab5f0a73acb9c585304c04c3539171a5f196
Jonathan Boisclair <jonathan.boisclair@uqtr.ca>
2024-03-06 15:10:21 -0500

    Performance: Allow lazy placeholders (#73)

--- plugin/src/main/java/com/iridium/iridiumcore/utils/Placeholder.java
+++ plugin/src/main/java/com/iridium/iridiumcore/utils/Placeholder.java
@@ -1,5 +1,7 @@
 package com.iridium.iridiumcore.utils;
 
+import java.util.function.Supplier;
+
 import lombok.Getter;
 
 /**
@@ -9,7 +11,8 @@ import lombok.Getter;
 public class Placeholder {
 
     private final String key;
-    private final String value;
+    private String value;
+    private Supplier<String> supplier;
 
     /**
      * The default constructor.
@@ -23,14 +26,36 @@ public class Placeholder {
     }
 
     /**
-     * Replaces this placeholder in the provided line with the value of this placeholder.
+     * The default constructor.
+     *
+     * @param key   The placeholder without curly brackets.
+     * @param value The actual value of the placeholder
+     */
+    public Placeholder(String key, Supplier<String> supplier) {
+        this.key = "%" + key + "%";
+        this.supplier = supplier;
+    }
+
+    public String getValue()
+    {
+        if(value==null && supplier!=null) value = supplier.get();
+        return value; 
+    }
+    /**
+     * Replaces this placeholder in the provided line with the value of this
+     * placeholder.
      *
      * @param line The line which potentially contains the placeholder
-     * @return The processed line with this placeholder replaced, empty if the parameter is null
+     * @return The processed line with this placeholder replaced, empty if the
+     *         parameter is null
      */
     public String process(String line) {
-        if (line == null || line.isEmpty()) return "";
-        return line.replace(key, value);
+        if (line == null || line.isEmpty())
+            return "";
+        if (line.contains(key))
+            return line.replace(key, getValue());
+        else
+            return line;
     }
 
 }
