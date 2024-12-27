3ee991679af454e96e8c6b484356889f8f517212
Peaches_MLG <PeachesMLG100@gmail.com>
2024-12-27 22:44:07 +0000

    Bump Spigot API

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
 
     // Other dependencies that are not required or already available at runtime
     compileOnly("org.jetbrains:annotations:26.0.1")
-    compileOnly("org.spigotmc:spigot-api:1.21.1-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.21.4-R0.1-SNAPSHOT")
 }
 
 tasks {
